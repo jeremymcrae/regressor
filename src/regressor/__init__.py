1a9bab7a6a3ca2089273a19e6aa6390640f496da
Jeremy McRae <jmcrae@illumina.com>
2021-09-22 15:17:50 -0700

    if mkl is installed, only use one thread

--- src/regressor/__init__.py
+++ src/regressor/__init__.py
@@ -2,4 +2,10 @@ from pkg_resources import get_distribution
 
 __version__ = get_distribution('regressor').version
 
+try:
+    import mkl
+    mkl.set_num_threads(1)
+except ImportError:
+    pass
+
 from regressor.regress import linregress
